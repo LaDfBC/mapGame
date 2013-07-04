bce31de7e2ce8ea0e7b491bd6436efc40d8986e4
George <george@ubuntu.ubuntu-domain>
2013-07-03 23:08:52 -0500

    Initial Source Push

---
+++ graph.cpp
@@ -0,0 +1,11 @@
+template <class T>
+Graph<T>::Graph()
+{
+	size = 0;
+}
+
+template <class T>
+Graph<T>::Graph(int input_size)
+{
+	size = input_size;
+}
---
+++ graph.h
@@ -0,0 +1,14 @@
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+template <class T>
+class Graph
+{
+  public:
+	  Graph();
+	  Graph(int count);
+  private:
+	  unsigned short size;
+};
+
+#endif /*GRAPH_H_*/
