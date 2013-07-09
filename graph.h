3c8e0bf06db1d27a58b2e02d3cf127116a4fcb17
George <george@ubuntu.ubuntu-domain>
2013-07-08 20:28:06 -0500

    List half done

--- graph.h
+++ graph.h
@@ -1,14 +1,30 @@
-#ifndef GRAPH_H_
-#define GRAPH_H_
+/* Programmer: George Mausshardt
+ * 
+ * Purpose: Header File for a graph implementation
+ * File: graph.h
+ */
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include "templates.h"
 
 template <class T>
 class Graph
 {
   public:
 	  Graph();
-	  Graph(int count);
+	  Graph(const Graph<T>& input_graph);
+	  ~Graph()
+	  
+	  Graph<T>& operator=(const Graph<T>& input_graph);
+	  nsize size(){ return m_size; }
   private:
-	  unsigned short size;
+	  nsize m_size;
+	  Graph_Node m_root;
+	  
+	  copy(const Graph<T> input_graph);
 };
 
+#include "graph.hpp"
 #endif /*GRAPH_H_*/
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
+#include "list.h"
+#include <iostream>
 
-#include "graph.h"
+using std::cout;
+using std::endl;
 
 int main()
 {
-	Graph();
+	List<short> myList(20);
+	myList.append(21);
+	myList.append(21);
+	myList.append(21);
+	cout << myList[0] << endl;
+	myList.clear();
+	cout << "size: " << myList.size() << endl;
 	return 0;
 }
 
